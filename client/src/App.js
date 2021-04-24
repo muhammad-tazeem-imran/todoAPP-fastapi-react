d9b55713b2d005dd2ffa06f8157fa509da0e522a
tazeem <tazeem.imran@arbisoft.com>
2021-04-25 02:23:17 +0500

    :lipstick: Modify the overall styles of the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import AddNewCard from './components/AddNew';
+import Card from './components/Card';
+import TitleBar from './components/TitleBar';
+import styles from './App.module.scss'
+import './theme/theme.scss';
+
 export default function App() {
   return (
-    <div>Main app here</div>
+    <div className={styles.main}>
+      <TitleBar text="Todo App" />
+
+      <div className={styles.cardList}>
+        <AddNewCard />
+        <Card title="First TODO"/>
+        <Card title="First TODO"/>
+        <Card title="First TODO"/>
+        <Card title="First TODO"/>
+      </div>
+    </div>
   );
 }
